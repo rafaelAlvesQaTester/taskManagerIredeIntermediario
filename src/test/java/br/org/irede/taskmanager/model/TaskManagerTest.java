998f149e21a4117d064a03def57b9bea1fd82ecd
Your Name <rafaelalvestester@gmail.com>
2026-09-07 22:05:05 -0300

    Consolida testes na classe TaskManagerTest

--- src/test/java/br/org/irede/taskmanager/model/TaskManagerTest.java
+++ src/test/java/br/org/irede/taskmanager/model/TaskManagerTest.java
@@ -1,5 +1,6 @@
 package br.org.irede.taskmanager.model;
 
+import br.org.irede.taskmanager.exception.EntradaInvalidaException;
 import br.org.irede.taskmanager.exception.TarefaJaConcluidaException;
 import br.org.irede.taskmanager.exception.TarefaNaoEncontradaException;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TaskManagerTest {
@@ -106,4 +108,72 @@ class TaskManagerTest {
                 () -> manager.removerTarefa(99)
         );
     }
+
+        @Test
+        void deveSalvarTituloDescricaoETarefaConcluida() {
+                Tarefa tarefa = new Tarefa(1, "Fazer churrasco", "Comprar carnes");
+                tarefa.setConcluida(true);
+
+                assertDoesNotThrow(tarefa::validar);
+                assertEquals(StatusTarefa.CONCLUIDA, tarefa.getStatus());
+        }
+
+        @Test
+        void deveAdvertirQuandoTituloEstiverVazioComDescricaoEConcluida() {
+                Tarefa tarefa = new Tarefa(1, "", "Comprar carnes");
+                tarefa.setConcluida(true);
+
+                EntradaInvalidaException exception = assertThrows(
+                                EntradaInvalidaException.class,
+                                tarefa::validar
+                );
+
+                assertEquals("Informe o titulo da tarefa.", exception.getMessage());
+        }
+
+        @Test
+        void deveAdvertirQuandoTituloEDescricaoEstiveremVaziosEConcluida() {
+                Tarefa tarefa = new Tarefa(1, "", "");
+                tarefa.setConcluida(true);
+
+                EntradaInvalidaException exception = assertThrows(
+                                EntradaInvalidaException.class,
+                                tarefa::validar
+                );
+
+                assertEquals("Informe o titulo da tarefa.", exception.getMessage());
+        }
+
+        @Test
+        void deveAdvertirQuandoTituloEDescricaoEstiveremVaziosEPendente() {
+                Tarefa tarefa = new Tarefa(1, "", "");
+
+                EntradaInvalidaException exception = assertThrows(
+                                EntradaInvalidaException.class,
+                                tarefa::validar
+                );
+
+                assertEquals("Informe o titulo da tarefa.", exception.getMessage());
+                assertEquals(StatusTarefa.PENDENTE, tarefa.getStatus());
+        }
+
+        @Test
+        void deveAceitarSomenteTituloComTarefaPendente() {
+                Tarefa tarefa = new Tarefa(1, "Fazer churrasco", "");
+
+                assertDoesNotThrow(tarefa::validar);
+                assertEquals(StatusTarefa.PENDENTE, tarefa.getStatus());
+        }
+
+        @Test
+        void deveAceitarTituloEDescricaoSemSelecionarConclusao() {
+                Tarefa tarefa = new Tarefa(
+                                1,
+                                "Fazer churrasco",
+                                "Comprar carnes e bebidas"
+                );
+
+                assertDoesNotThrow(tarefa::validar);
+                assertEquals(StatusTarefa.PENDENTE, tarefa.getStatus());
+        }
 }
